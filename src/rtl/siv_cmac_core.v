b33f81cf95fc44512085451269fc201921ba493a
Joachim Strömbergson <joachim@assured.se>
2019-07-25 15:34:39 +0200

    Adding initial version of the core module.

---
+++ src/rtl/siv_cmac_core.v
@@ -0,0 +1,157 @@
+//======================================================================
+//
+// siv_cmac_core.v
+// ---------------
+// Implementation av aead_aes_siv_cmac.
+//
+//
+// Author: Joachim Strombergson
+// Copyright (c) 2019, Assured AB
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//======================================================================
+
+module siv_cmac_core(
+                     input wire            clk,
+                     input wire            reset_n,
+
+                     input wire            init,
+                     input wire            next,
+                     input wire            finalize,
+
+                     input wire            encdec,
+                     input wire [511 : 0]  key,
+                     input wire            keylen,
+
+                     input wire [127 : 0]  block,
+
+                     output wire           ready,
+                     output wire [127 : 0] result,
+                     output wire [127 : 0] tag,
+                    );
+
+
+  //----------------------------------------------------------------
+  // Internal constant and parameter definitions.
+  //----------------------------------------------------------------
+  localparam CTRL_IDLE   = 2'h0;
+  localparam CTRL_NEXT   = 2'h1;
+  localparam CTRL_LOOP   = 2'h2;
+  localparam CTRL_FINISH = 2'h3;
+
+
+  //----------------------------------------------------------------
+  // Registers including update variables and write enable.
+  //----------------------------------------------------------------
+  reg          ready_reg;
+  reg          ready_new;
+  reg          ready_we;
+
+  reg [1 : 0]  core_ctrl_reg;
+  reg [1 : 0]  core_ctrl_new;
+  reg          core_ctrl_we;
+
+
+  //----------------------------------------------------------------
+  // Wires.
+  //----------------------------------------------------------------
+
+
+  //----------------------------------------------------------------
+  // Concurrent connectivity for ports etc.
+  //----------------------------------------------------------------
+  assign ready  = ready_reg;
+  assign result = 128'h0;
+  assign digest = 128'h0;
+
+
+  //----------------------------------------------------------------
+  // reg_update
+  //
+  // Update functionality for all registers in the core.
+  // All registers are positive edge triggered with synchronous
+  // active low reset.
+  //----------------------------------------------------------------
+  always @ (posedge clk)
+    begin: reg_update
+      if (!reset_n)
+        begin
+          ready_reg     <= 1'h1;
+          core_ctrl_reg <= CTRL_IDLE;
+        end
+      else
+        begin
+          if (ready_we)
+            ready_reg <= ready_new;
+
+          if (core_ctrl_we)
+            core_ctrl_reg <= core_ctrl_new;
+        end
+    end // reg_update
+
+
+  //----------------------------------------------------------------
+  // core_ctrl
+  //----------------------------------------------------------------
+  always @*
+    begin : core_ctrl
+      ready_new     = 1'h0;
+      ready_we      = 1'h0;
+      core_ctrl_new = CTRL_IDLE;
+      core_ctrl_we  = 1'h0;
+
+
+      case (core_ctrl_reg)
+        CTRL_IDLE:
+          begin
+            if (init)
+              begin
+              end
+
+            if (next)
+              begin
+              end
+
+
+            if (finalize)
+              begin
+              end
+          end
+
+        default:
+          begin
+
+          end
+      endcase // case (core_ctrl_reg)
+
+    end // core_ctrl
+endmodule // siv_cmac_core
+
+//======================================================================
+// EOF siv_cmac_core.v
+//======================================================================
